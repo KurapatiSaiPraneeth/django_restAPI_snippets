a9000ff5631611c2249337f1b0a7eee99c744b15
sai praneeth <kurapatisaipraneeth@gmail.com>
2018-11-01 11:27:35 +0530

    second commit

--- practice/snippets/views.py
+++ practice/snippets/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import
-
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
@@ -9,6 +7,10 @@ from .serializers import SnippetSerializer
 
 @csrf_exempt
 def snippet_list(request):
+
+    """
+    List all code snippets, or create a new snippet.
+    """
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets,many = True)
@@ -34,4 +36,16 @@ def snippet_detail(request, pk):
     except Snippet.DoesNotExist:
         return HttpResponse(status=404)
 
-    if re
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status =400)
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status = 204)
